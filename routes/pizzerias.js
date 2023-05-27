76c9cb36409f25a805e1ec52bbf205d92bcf1773
Cherch <b.cherchiglia@gmail.com>
2023-05-27 19:09:47 -0400

    Index Table lives! Thanks a tag

--- routes/pizzerias.js
+++ routes/pizzerias.js
@@ -11,6 +11,10 @@ const pizzeriasCtrl = require('../controllers/pizzerias');
 // a new pizzeria to Slice
 router.get('/new', pizzeriasCtrl.new);
 
+// GET /pizzerias/:id (SHOW functionality) 
+// this MUST be below the GET NEW Route
+// router.get('/:id', pizzeriasCtrl.show);
+
 // POST Route for /pizzerias
 // add newly created pizzeria to /pizzerias
 router.post('/', pizzeriasCtrl.create);
