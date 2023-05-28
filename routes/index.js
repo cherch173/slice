de7e3a553142f31b4b6893b9b3d35ce649494faf
Cherch <b.cherchiglia@gmail.com>
2023-05-28 14:03:22 -0400

    auth route added to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Slice' });
 });
+
+// Google OAuth Login Route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'], }
+));
 
 module.exports = router;
