60c448c3a5a134cc857ef2b99e857e965ac3426a
Cherch <b.cherchiglia@gmail.com>
2023-05-28 13:40:42 -0400

    passport config passport.use create async function

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,26 @@
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user')
 
+passport.use(new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK
+    },
+    async function(accessToken, refreshToken, profile, cb) {
+      // A user has logged in with OAuth...
+      try {
+       let user = await User.findOne({ googleId: profile.ed});
+       if (user) return cb(null, user);
+       user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+       })
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  ));
