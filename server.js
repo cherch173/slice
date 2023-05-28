0f47266c3a114523a480a200b94f9f7c9699e8f7
Cherch <b.cherchiglia@gmail.com>
2023-05-28 12:58:10 -0400

    session middleware server contd

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Session Middleware
+var session = require('express-session');
+
 // dotenv configured for security
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
@@ -23,6 +26,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
