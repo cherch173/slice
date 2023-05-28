b701cda0a4931be6a50aa409ab9b26e040fb137f
Cherch <b.cherchiglia@gmail.com>
2023-05-28 13:48:16 -0400

    deserialize user function complete

--- config/passport.js
+++ config/passport.js
@@ -29,3 +29,7 @@ passport.use(new GoogleStrategy(
   passport.serializeUser(function(user, cb) {
     cb(null, user._id);
   });
+
+  passport.deserializeUser(async function(userId, cb) {
+    cb(null, await User.findById(userId));
+  })
