29fd5a6425141c1bdc50b401401613feebe45871
Cherch <b.cherchiglia@gmail.com>
2023-05-28 14:50:44 -0400

    User Info added to Create Function in Reviews Controller for New Comments

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,6 +6,12 @@ module.exports = {
 
 async function create(req, res) {
     const pizzeria = await Pizzeria.findById(req.params.id);
+    // the user-centric data aka
+    // the newly added review-ski
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+
     pizzeria.reviews.push(req.body);
     try {
         await pizzeria.save();
